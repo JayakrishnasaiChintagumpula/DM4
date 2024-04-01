92d7ce7b6e9a0ae7e4253f7818054a57c76c0379
JayakrishnasaiChintagumpula <146905031+JayakrishnasaiChintagumpula@users.noreply.github.com>
2024-03-31 21:09:44 -0400

    Update part2.py

--- part2.py
+++ part2.py
@@ -36,22 +36,34 @@ In this task you will explore different methods to find a good value for k
 # Change the arguments and return according to 
 # the question asked. 
 
-def fit_kmeans(X, n_clusters):
-    """
-    Fits a k-means clustering algorithm to the data X and explicitly calculates the SSE.
-    """
-    kmeans = KMeans(n_clusters=n_clusters, random_state=12)
-    kmeans.fit(X)
-    labels = kmeans.labels_
-    centroids = kmeans.cluster_centers_
+def fit_kmeans(X, max_k):
+    sse_inertia = []
+    sse_manual = []
+    model_sse_inertia = {}
+    model_sse_manual = {}
     
-    # Explicit calculation of SSE
-    sse = 0
-    for i in range(n_clusters):
-        cluster_points = X[labels == i]
-        for point in cluster_points:
-            sse += np.sum((point - centroids[i]) ** 2)
-    return sse
+    for clusters_num in range(1, max_k + 1):
+        kmeans = KMeans(n_clusters=clusters_num, random_state=12)
+        preds = kmeans.fit_predict(X)
+        inertia = kmeans.inertia_
+        sse_inertia.append(inertia)
+        
+        # Manual SSE calculation
+        sse = {}
+        for i in range(len(preds)):
+            point = X[i]
+            centroid = kmeans.cluster_centers_[preds[i]]
+            distance = (point - centroid) ** 2
+            sse_contrib = np.sum(distance)
+            sse[preds[i]] = sse.get(preds[i], 0) + sse_contrib
+        
+        sse_manual_value = sum(sse.values())
+        sse_manual.append(sse_manual_value)
+        
+        model_sse_inertia[clusters_num] = inertia
+        model_sse_manual[clusters_num] = sse_manual_value
+
+    return sse_inertia, sse_manual, model_sse_inertia, model_sse_manual
 
 
 
@@ -66,7 +78,10 @@ def compute():
 
     # dct: return value from the make_blobs function in sklearn, expressed as a list of three numpy arrays
     X, y = make_blobs(center_box=(-20, 20), n_samples=20, centers=5, random_state=12)
-    dct = answers["2A: blob"] = [X, y]
+
+    # Create an additional array, for example, a unique identifier for each sample
+    sample_ids = np.arange(1, len(X) + 1)
+    dct = answers["2A: blob"] = [X, y,sample_ids]
 
     """
     B. Modify the fit_kmeans function to return the SSE (see Equations 8.1 and 8.2 in the book).
@@ -81,22 +96,19 @@ def compute():
 
     # dct value: a list of tuples, e.g., [[0, 100.], [1, 200.]]
     # Each tuple is a (k, SSE) pair
-    sse_values = []
-    for k in range(1, 9):
-        sse = fit_kmeans(X, k)
-        sse_values.append(sse)
+    _, sse_values, _, _ = fit_kmeans(X, 8)
 
-    # Plotting the SSE values to determine the elbow
+    # Plotting the manually calculated SSE as a function of k
     plt.figure(figsize=(10, 6))
-    plt.plot(range(1, 9), sse_values, marker='o')
-    plt.title('Elbow Method for Determining Optimal k')
-    plt.xlabel('Number of clusters (k)')
+    plt.plot(range(1, 9), sse_values, marker='o', linestyle='-', color='green', label='SSE (Manual Calculation)')
+    plt.title('Elbow Method for Optimal k (Manual SSE Calculation)')
+    plt.xlabel('Number of Clusters k')
     plt.ylabel('Sum of Squared Errors (SSE)')
     plt.xticks(range(1, 9))
+    plt.legend()
     plt.grid(True)
     plt.show()
-
-    return sse_values
+    
     print(sse_values)
     dct = answers["2C: SSE plot"] = sse_values
 
@@ -105,18 +117,19 @@ def compute():
     """
 
     # dct value has the same structure as in 2C
-    inertia_values = []
-    for k in range(1, 9):
-        inertia = fit_kmeans_and_get_inertia(X, k)
-        inertia_values.append(inertia)
+    _, _, model_sse_inertia, _ = fit_kmeans(X, 8)
 
-    # Plotting the inertia values to determine the elbow
+    # Extracting the inertia values for plotting
+    inertia_values = list(model_sse_inertia.values())
+    
+    # Plotting the inertia values as a function of k
     plt.figure(figsize=(10, 6))
-    plt.plot(range(1, 9), inertia_values, marker='o', color='red')
-    plt.title('Elbow Method for Determining Optimal k using Inertia')
-    plt.xlabel('Number of clusters (k)')
-    plt.ylabel('Inertia')
+    plt.plot(range(1, 9), inertia_values, marker='o', linestyle='-', color='blue', label='Inertia')
+    plt.title('Inertia for Different Numbers of Clusters k')
+    plt.xlabel('Number of Clusters k')
+    plt.ylabel('Inertia (SSE)')
     plt.xticks(range(1, 9))
+    plt.legend()
     plt.grid(True)
     plt.show()
     dct = answers["2D: inertia plot"] = inertia_values
